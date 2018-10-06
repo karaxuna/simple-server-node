4ac7cf20224f61e3351673dd40f83641d7f37f75
Kakhaber <kaxabernet@gmail.com>
2018-10-06 20:18:31 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.send('Hello World9!');
+  res.send('Hello World0!');
 });
 
 //start a server on port 80 and log its start to our console
